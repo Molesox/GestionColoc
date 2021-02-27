eca9f12c88c2e0ee7cace5b822d6797ff0f3e03e
Molesox <sanz.daniel95@gmail.com>
2021-02-27 13:57:12 +0100

    Ajoutez des fichiers projet.

---
+++ GestionColoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GestionColoc.MvcApplication" Language="C#" %>
---
+++ GestionColoc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionColoc.Startup))]
+namespace GestionColoc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
